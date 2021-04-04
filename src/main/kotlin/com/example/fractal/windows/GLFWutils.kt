ce7c38c2ae1df29a2decaec3eba5237281a02c29
edipowladeo <edipo.as@gmail.com>
2021-04-03 23:12:35 -0300

    code gardening

--- src/main/kotlin/com/example/fractal/windows/GLFWutils.kt
+++ src/main/kotlin/com/example/fractal/windows/GLFWutils.kt
@@ -3,20 +3,18 @@ package com.example.fractal.windows
 import com.example.fractal.CoordenadasTelai
 import com.example.fractal.WindowHandle
 import org.lwjgl.glfw.GLFW
-import org.lwjgl.system.MemoryStack
-import java.nio.IntBuffer
 
 fun getDesktopResolution(): CoordenadasTelai{
-    val vidmode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor())!!    // Get the resolution of the primary monitor
+    val videoMode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor())!!    // Get the resolution of the primary monitor
 
-    return CoordenadasTelai(vidmode.width(),vidmode.height())
+    return CoordenadasTelai(videoMode.width(),videoMode.height())
 }
 
 fun getOGLWindowResolution(windowHandle: WindowHandle): CoordenadasTelai{
-    var x :IntArray = IntArray(1)
-    var y :IntArray = IntArray(1)
+    val x  = IntArray(1)
+    val y  = IntArray(1)
 
     GLFW.glfwGetWindowSize(windowHandle, x, y)
 
-    return CoordenadasTelai(x?.get(0)!!, y?.get(0)!!)
+    return CoordenadasTelai(x[0], y[0])
 }
